ceb0db65d486112b95c821dab8bf576e0f8aa140
Pasan Madushan <pasanmadushan@gmail.com>
2023-02-10 13:55:35 +0530

    db setup

--- server.js
+++ server.js
@@ -1,24 +1,37 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const mysql = require("mysql");
 
-// var cors = require("cors");
-// const bodyParser = require('body-parser')
-
+require("dotenv").config();
 
 const app = express();
 
-//   app.use(express.json());
-//   app.use(cors());
-//   app.use(bodyParser.urlencoded({extended:true}))
-// app.use(bodyParser.json())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
   
 //   app.use("/", require("./routes"));
 
-
-app.get('/cat',(req,res)=>{
-    res.send('Bye')
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"auto-choice",
+    password:"1234",
+    database:"auto-choice"
 })
 
+db.connect((err)=>{
+    if(err) throw err;
+    console.log("Database connection successful")
+});
 
-app.listen(3000, () => {
-    console.log("Server is running on", 3000);
+app.get('/cat',(req,res)=>{
+    let sql = 'SELECT * from garage';
+    db.query(sql,(err,result)=>{
+        if(err) throw err;
+        console.log(result);
+        res.send(result);
+    })
+})
+
+app.listen(process.env.PORT, () => {
+    console.log("Server is running on", process.env.PORT);
 });
